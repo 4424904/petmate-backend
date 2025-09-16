2a1d7fde2d8f115b1e2aa6290628ba9899ba9729
hyunee507 <hyunee507@gmail.com>
2025-09-16 16:58:26 +0900

    [20250916_03_KKH] 상품관리 데이터 연동

--- src/main/java/com/petmate/domain/product/repository/jpa/ProductRepository.java
+++ src/main/java/com/petmate/domain/product/repository/jpa/ProductRepository.java
@@ -11,13 +11,19 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 
+    // 특정 업체들의 상품 조회
+    List<ProductEntity> findByCompanyIdIn(List<Integer> companyIds);
+
+    // 단일 업체 상품조회
     List<ProductEntity> findByCompanyId(Integer companyId);
-    List<ProductEntity> findByServiceType(String serviceType);
-    List<ProductEntity> findByIsActive(Integer isActive);
+//    List<ProductEntity> findByServiceType(String serviceType);
+//    List<ProductEntity> findByIsActive(Integer isActive);
 
     @Query("SELECT p FROM ProductEntity p WHERE p.companyId = :companyId AND p.isActive = 1")
     List<ProductEntity> findActiveProductsByCompany(@Param("companyId") Integer companyId);
 
     // 가격 범위 검색
-    List<ProductEntity> findByPriceBetweenAndIsActive(Integer minPrice, Integer maxPrice, Integer isActive);
+//    List<ProductEntity> findByPriceBetweenAndIsActive(Integer minPrice, Integer maxPrice, Integer isActive);
+
+
 }
